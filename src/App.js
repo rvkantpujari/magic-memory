ab2303a71cfba33d400e2853ec12a09c60ecc9b0
Ravi Kant Pujari <rvkantpujari@gmail.com>
2022-05-15 23:55:18 -0400

    Card Component added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+import SingleCard from "./components/SingleCard.js";
 
 const cardImages = [
   { src: "/img/helmet-1.png" },
@@ -32,12 +33,7 @@ function App() {
       <button onClick={shuffleCards}>New Game</button>
       <div className="card-grid">
         {cards.map((card) => (
-          <div className="card" key={card.id}>
-            <div>
-              <img className="front" src={card.src} alt="front card" />
-              <img className="back" src="/img/cover.png" alt="back card" />
-            </div>
-          </div>
+          <SingleCard key={card.id} card={card} />
         ))}
       </div>
     </div>
